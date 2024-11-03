c89c79480672297e2bcbd6a0f6de88a40c557be5
Avijeet <avijeetpandey25@gmail.com>
2024-11-04 00:13:51 +0530

    add: min max array

---
+++ src/leetcode/MinAndMaxArray.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MinAndMaxArray {
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,56,10000,167};
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[arr.length-1]);
+    }
+}
