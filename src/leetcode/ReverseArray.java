083bda80180d81de02e45a7bc7b29e02cc2cc10e
Avijeet <avijeetpandey25@gmail.com>
2024-11-04 00:04:57 +0530

    add: reverse array

---
+++ src/leetcode/ReverseArray.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] elements = {1,2,3,4,5,6};
+        int left = 0;
+        int right = elements.length - 1;
+
+        for(int i=0;i< elements.length -1;i++){
+            int temp = elements[left];
+            elements[left] = elements[right];
+            elements[right] = temp;
+            left++;
+            right--;
+        }
+
+        System.out.println(Arrays.toString(elements));
+    }
+}
