96d1182f7c46fe8e01c4e383ba0905f76b38f942
Avijeet <avijeetpandey25@gmail.com>
2024-11-16 22:41:55 +0530

    add: IndexOfFirstOccurence

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
-import ds.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.add(12);
-        list.add(13);
-        list.add(14);
-        list.add(15);
-        list.add(16);
-        list.print();
-        System.out.println(list.find(14));
-        System.out.println(list.find(30));
+
     }
 }
---
+++ src/leetcode/IndexOfFirstOccurence.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class IndexOfFirstOccurence {
+    public static void main(String[] args) {
+        String hayStack = "leetcode";
+        String needle = "leetc";
+
+        int index = hayStack.indexOf(needle);
+        System.out.println(index);
+    }
+}
