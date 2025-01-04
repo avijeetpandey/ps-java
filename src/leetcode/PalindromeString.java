2888a9fc38cfd95e3875b2afe67906867516eb00
Avijeet <avijeetpandey25@gmail.com>
2025-01-04 23:38:29 +0530

    Add: palindrome string

---
+++ src/leetcode/PalindromeString.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s = "abc";
+        int start = 0;
+        int end = s.length() - 1;
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(s.charAt(start) != s.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println(isPalindrome);
+    }
+}
