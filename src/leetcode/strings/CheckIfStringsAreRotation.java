2ab269962971ece6aa40332cbcb7c4cced89884c
Avijeet <avijeetpandey25@gmail.com>
2025-01-05 00:06:53 +0530

    string rotation

---
+++ src/leetcode/strings/CheckIfStringsAreRotation.java
@@ -0,0 +1,34 @@
+package leetcode.strings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckIfStringsAreRotation {
+    public static void main(String[] args) {
+        String one = "mightandmagic";
+        String two = "andmagicmigth";
+        boolean isRotated = true;
+
+        Map<Character, Integer> firstMap = new HashMap<>();
+        Map<Character, Integer> secondMap = new HashMap<>();
+
+        for(char c: one.toCharArray()) {
+            firstMap.put(c, firstMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c: two.toCharArray()) {
+            secondMap.put(c, secondMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(Map.Entry<Character, Integer> entry: firstMap.entrySet()) {
+            if(entry.getValue() != secondMap.get(entry.getKey())) {
+                isRotated = false;
+                break;
+            }
+        }
+
+        System.out.println(firstMap);
+        System.out.println(secondMap);
+    }
+}
