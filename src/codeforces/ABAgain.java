fd2ba517a211e098bc56880e90e3e978095f3abb
Avijeet <avijeetpandey25@gmail.com>
2025-02-27 01:16:48 +0530

    add: 1999A

---
+++ src/codeforces/ABAgain.java
@@ -0,0 +1,23 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class ABAgain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t > 0) {
+            int n = scanner.nextInt();
+
+            int firstDigit = n%10;
+            n = n/10;
+
+            System.out.println(n+firstDigit);
+
+            t--;
+        }
+
+        scanner.close();
+    }
+}
