d3aa2099156164124ef59e7375a4c688fbc86e30
Avijeet <avijeetpandey25@gmail.com>
2025-02-27 00:38:45 +0530

    add: stream api example

---
+++ src/streams/README.md
@@ -0,0 +1,4 @@
+## Streams in Java
+Streams are introduced in Java8, they are basically used to process collection objects, a stream in java is a sequence of objects that support methods that can be
+piped to produce the desired outputs.
+
---
+++ src/streams/Streams.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Streams {
+    public static void main(String[] args) {
+        List<List<String>> listOfLists = Arrays.asList(
+                Arrays.asList("Reflection", "Collection", "Stream"),
+                Arrays.asList("Structure", "State", "Flow"),
+                Arrays.asList("Sorting", "Mapping", "Reduction", "Stream")
+        );
+
+        Set<String> intermediateResults = new HashSet<>();
+
+        List<String> result = listOfLists.stream()
+                .flatMap(List::stream)
+                .filter(s -> s.startsWith("S"))
+                .map(String::toUpperCase)
+                .distinct()
+                .sorted()
+                .peek(intermediateResults::add)
+                .toList();
+
+        System.out.println("Intermediate results");
+        result.forEach(System.out::println);
+    }
+}
