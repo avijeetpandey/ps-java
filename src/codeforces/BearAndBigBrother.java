9ace67b0495d474f634e85d80ceaecc46e5e5f7d
Avijeet <avijeetpandey25@gmail.com>
2025-03-04 22:59:43 +0530

    add: 791A

---
+++ src/codeforces/BearAndBigBrother.java
@@ -0,0 +1,24 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class BearAndBigBrother {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int limakWeight = scanner.nextInt();
+        int bobsWeight = scanner.nextInt();
+
+        int count = 0;
+
+        while(limakWeight <= bobsWeight) {
+            limakWeight *= 3;
+            bobsWeight *= 2;
+            count++;
+        }
+
+        System.out.println(count);
+
+        scanner.close();
+    }
+}
