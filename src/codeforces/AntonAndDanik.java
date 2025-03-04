ba4c2358f93665fdb00a62bcd77d277f5781a2c3
Avijeet <avijeetpandey25@gmail.com>
2025-03-04 23:23:34 +0530

    add: 734A

---
+++ src/codeforces/AntonAndDanik.java
@@ -0,0 +1,32 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class AntonAndDanik {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String gameState = in.next();
+
+        int antonCount = 0;
+        int danikCount = 0;
+
+        for(char c : gameState.toCharArray()) {
+            if(c == 'A') {
+                antonCount++;
+            } else if(c == 'D') {
+                danikCount++;
+            }
+        }
+
+        if(antonCount > danikCount) {
+            System.out.println("Anton");
+        } else if(danikCount > antonCount) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+
+        in.close();
+    }
+}
