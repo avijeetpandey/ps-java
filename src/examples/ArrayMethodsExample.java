5d98193a97b52b62dcc50a1d84f1ee2d548389c5
Avijeet Pandey <avijeetpandey25@gmail.com>
2025-12-12 23:51:12 +0530

    remove unused imports

--- src/examples/ArrayMethodsExample.java
+++ src/examples/ArrayMethodsExample.java
@@ -2,7 +2,6 @@ package examples;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ArrayMethodsExample {
@@ -13,21 +12,20 @@ public class ArrayMethodsExample {
         list.add(2);
         list.add(3);
 
-
         // adding multiple elements at the same time
-        List<Integer> bulkList = Arrays.asList(1,2,3,4,5);
+        List<Integer> bulkList = Arrays.asList(1, 2, 3, 4, 5);
 
         System.out.println(list);
         System.out.println(bulkList);
 
         // sorting the array
-        int[] unsortedList = new int[] {20,6,-12,90,88,100};
+        int[] unsortedList = new int[] { 20, 6, -12, 90, 88, 100 };
         Arrays.sort(unsortedList);
         System.out.println(Arrays.toString(unsortedList));
 
         // sorting to a certain range
-        int[] unsortedRangeList = new int[] {20,6,-12,90,88,100};
-        Arrays.sort(unsortedRangeList,2, unsortedRangeList.length-1);
+        int[] unsortedRangeList = new int[] { 20, 6, -12, 90, 88, 100 };
+        Arrays.sort(unsortedRangeList, 2, unsortedRangeList.length - 1);
         System.out.println(Arrays.toString(unsortedRangeList));
     }
 }
--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -4,15 +4,13 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Streams {
     public static void main(String[] args) {
         List<List<String>> listOfLists = Arrays.asList(
                 Arrays.asList("Reflection", "Collection", "Stream"),
                 Arrays.asList("Structure", "State", "Flow"),
-                Arrays.asList("Sorting", "Mapping", "Reduction", "Stream")
-        );
+                Arrays.asList("Sorting", "Mapping", "Reduction", "Stream"));
 
         Set<String> intermediateResults = new HashSet<>();
 
