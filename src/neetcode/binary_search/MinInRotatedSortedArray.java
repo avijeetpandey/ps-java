cfe70fe2c37b3982e68f7758a2ca49804f520055
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-02-21 17:23:08 +0530

    Add: min in rotated sorted array

---
+++ src/neetcode/binary_search/MinInRotatedSortedArray.java
@@ -0,0 +1,24 @@
+package neetcode.binary_search;
+
+public class MinInRotatedSortedArray {
+
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int mid = (int) Math.floor((left+right)/2);
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[right];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(findMin(nums));
+    }
+}
