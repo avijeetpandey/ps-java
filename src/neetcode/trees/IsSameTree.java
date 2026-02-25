dff5fcff201155a623f5059fdad005f706865778
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-02-25 16:36:07 +0530

    add: is same tree

---
+++ src/neetcode/trees/IsSameTree.java
@@ -0,0 +1,15 @@
+package neetcode.trees;
+
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+
+        if(p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
