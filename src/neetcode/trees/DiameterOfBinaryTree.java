4841d344bcab3cd5d3decf290f26792d10793307
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-02-25 14:41:00 +0530

    add: max depth and diameter of binary tree

---
+++ src/neetcode/trees/DiameterOfBinaryTree.java
@@ -0,0 +1,29 @@
+package neetcode.trees;
+
+public class DiameterOfBinaryTree {
+    public int diameter(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+
+        int leftHeight = heightOfTree(root.left);
+        int rightHeight = heightOfTree(root.right);
+
+        int diameterAtLevel = leftHeight + rightHeight;
+
+        int subDiameter = Math.max(diameter(root.left), diameter(root.right));
+
+        return Math.max(diameterAtLevel, subDiameter);
+    }
+
+    public int heightOfTree(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int rightHeight = heightOfTree(root.right);
+        int leftHeight = heightOfTree(root.left);
+
+        return 1 + Math.max(rightHeight, leftHeight);
+    }
+}
