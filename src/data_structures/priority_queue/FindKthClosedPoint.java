0ca821d9abfaf7a3c20a203206c30298214de1bf
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-03-26 21:27:01 +0530

    fix: error

--- src/data_structures/priority_queue/FindKthClosedPoint.java
+++ src/data_structures/priority_queue/FindKthClosedPoint.java
@@ -3,7 +3,7 @@ package data_structures.priority_queue;
 import java.util.PriorityQueue;
 
 public class FindKthClosedPoint {
-    public int[] kthClosest(int[][] points, int k) {
+    public int[][] kthClosest(int[][] points, int k) {
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> {
             int distA = a[0]*a[0] + a[1]*a[1];
             int distB = b[0]*b[0] + b[1]*b[1];
